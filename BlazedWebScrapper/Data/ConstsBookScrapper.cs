51f745fcb78eca031959e72853d7d44afb0ea72a
Kamil Mikuła <kamimik090@student.polsl.pl>
2024-10-26 19:51:47 +0200

    Added consts

---
+++ BlazedWebScrapper/Data/ConstsBookScrapper.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages.Infrastructure;
+
+namespace BlazedWebScrapper.Data
+{
+    public class ConstsBookScrapper
+    {
+        const string productWrapper = "product s-grid-3 product-main-wrap";
+        const string aTag = "a";
+        const string divTag = "div";
+        const string classAttribute = "class";
+        public string ProductWrapper { get => productWrapper;}
+        public string ATag { get => aTag; }
+        public string DivTag { get => divTag; }
+        public string ClassAttribute { get => classAttribute; }
+    }
+}
