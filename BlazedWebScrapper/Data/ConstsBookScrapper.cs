058c1a8a372f8b2cc204add423cfd7739a9f345a
Kamil Mikuła <kamimik090@student.polsl.pl>
2024-10-27 22:00:19 +0100

    New component and search result view

--- BlazedWebScrapper/Data/ConstsBookScrapper.cs
+++ BlazedWebScrapper/Data/ConstsBookScrapper.cs
@@ -3,14 +3,20 @@
 namespace BlazedWebScrapper.Data
 {
     public class ConstsBookScrapper
-    {
+    {    //Wydawnictwoniezwykle.
+         //ksiegarnia.pwn.pl
+         //nk.com.pl
+         //albatros (ciężkie)
+         //czytelnik.pl/
         const string productWrapper = "product s-grid-3 product-main-wrap";
         const string aTag = "a";
         const string divTag = "div";
         const string classAttribute = "class";
+        const string czytelnikSite = "https://czytelnik.pl/pl/searchquery/";
         public string ProductWrapper { get => productWrapper;}
         public string ATag { get => aTag; }
         public string DivTag { get => divTag; }
         public string ClassAttribute { get => classAttribute; }
+        public string CzytelnikSite { get => czytelnikSite;}
     }
 }
