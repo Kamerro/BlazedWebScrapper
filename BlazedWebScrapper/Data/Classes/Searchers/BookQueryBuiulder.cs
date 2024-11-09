3acb64da976eae83d070994156a4040bc042518e
Kamil Mikuła <kamimik090@student.polsl.pl>
2024-11-09 12:37:50 +0100

    Added new services

---
+++ BlazedWebScrapper/Data/Classes/Searchers/BookQueryBuiulder.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+
+namespace BlazedWebScrapper.Data.Classes.Searchers
+{
+    internal class BookQueryBuiulder
+    {
+        internal string BuildObjectOfInterest(string inputValue, string authorName, string title)
+        {
+
+            StringBuilder sb = new StringBuilder();
+
+            if (string.IsNullOrEmpty(inputValue))
+            {
+                sb.Append(authorName);
+                sb.Append(" ");
+                sb.Append(title);
+            }
+            else
+            {
+                sb.Append(inputValue);
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ BlazedWebScrapper/Data/Classes/Searchers/LinkService.cs
@@ -0,0 +1,22 @@
+﻿namespace BlazedWebScrapper.Data.Classes.Searchers
+{
+    internal class LinkService
+    {
+        private List<string> _links;
+        private string _PWNBase;
+
+        public LinkService(List<string> links, string pWNBase)
+        {
+            this._links = links;
+            this._PWNBase = pWNBase;
+        }
+
+        public void GenerateLinks()
+        {
+            for (int i = 0; i < _links.Count; i++)
+            {
+                _links[i] = _PWNBase + _links[i];
+            }
+        }
+    }
+}
