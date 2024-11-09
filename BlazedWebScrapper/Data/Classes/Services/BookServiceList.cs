0b3280cd8dcbfbbf163b76c9842779f2fed6992c
Kamil Mikuła <kamimik090@student.polsl.pl>
2024-11-09 12:39:16 +0100

    extend bookServiceList

--- BlazedWebScrapper/Data/Classes/Services/BookServiceList.cs
+++ BlazedWebScrapper/Data/Classes/Services/BookServiceList.cs
@@ -1,7 +1,28 @@
-﻿namespace BlazedWebScrapper.Data.Classes.Services
+﻿using BlazedWebScrapper.Data.Classes.Data;
+using System.Text.RegularExpressions;
+
+namespace BlazedWebScrapper.Data.Classes.Services
 {
     public class BookServiceList
     {
         public List<Tuple<string,decimal,string>> FullListOfBooks = new List<Tuple<string, decimal, string>>();
+
+        internal void GenerateFullListOfBooks(List<string> books, List<string> authors, List<string> links,List<string> prices)
+        {
+            for (int i = 0; i < books.Count && i < authors.Count && i < links.Count; i++)
+            {
+                var match = Regex.Match(prices[i], @"\d+([.,]\d{1,2})?");
+                if (match.Success)
+                {
+                    string filteredValue = match.Value.Replace(".", ",");
+
+                    if (decimal.TryParse(filteredValue, out decimal price))
+                    {
+                        FullListOfBooks.Add(new($"{authors[i]}-{books[i]}", price, links[i]));
+                    }
+                }
+
+            }
+        }
     }
 }
